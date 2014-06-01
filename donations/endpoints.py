5267f1a0f6f1825044912edfac1413001d5be457
Kyle Benson <kyle.edward.benson@gmail.com>
2014-06-01 17:25:58 -0400

    removed [0]s that were not necessary, still no dice with mobile app

--- donations/endpoints.py
+++ donations/endpoints.py
@@ -9,24 +9,25 @@ def search(request):
     if request.method == 'POST' and len(request.POST):
         query = request.POST
         print query
+        print query['lat']
         #TODO: fix the JSON in the map.html file so it doesn't wrap each value in a list...
         objs = Donation.objects.filter(
-                                       time_window_end__gte=query['time_window_start'][0],
-                                       time_window_start__lte=query['time_window_end'][0],
-                                       location__distance_lte=(Point(float(query['lat'][0]), float(query['lon'][0])), Distance(mi=query['distance'][0])),
+                                       time_window_end__gte=query['time_window_start'],
+                                       time_window_start__lte=query['time_window_end'],
+                                       location__distance_lte=(Point(float(query['lat']), float(query['lon'])), Distance(mi=query['distance'])),
                                        )
 
-        if query['food_type'][0]:
-            objs = objs.filter(food_type__in=query['food_type'][0].strip().split())
+        if query['food_type']:
+            objs = objs.filter(food_type__in=query['food_type'].strip().split())
 
         # careful here, don't want to use exclude as a donation may have both
         # options.  Note that one of these must be true!
-        if not query['pickup'][0] or not query['dropoff'][0]:
-            objs = objs.filter(dropoff=True) if not query['pickup'][0] else objs.filter(pickup=True)
+        if not query['pickup'] or not query['dropoff']:
+            objs = objs.filter(dropoff=True) if not query['pickup'] else objs.filter(pickup=True)
 
-        if query['keywords'][0]:
+        if query['keywords']:
             kw_objs = Donation.objects.none()
-            for kw in query['keywords'][0].split():
+            for kw in query['keywords'].split():
                 kw_objs |= objs.filter(short_description__icontains=kw)
                 kw_objs |= objs.filter(long_description__icontains=kw)
             objs = kw_objs
@@ -34,4 +35,4 @@ def search(request):
         return encoder.encode(objs)
 
     else:
-        raise AJAXError(500, 'Nothing to echo back.')
+        raise AJAXError(5, 'Nothing to echo back.')
