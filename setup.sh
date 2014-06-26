a47ddea4055b74a9e15252acce666589428fbc13
Kyle Benson <kebenson@ics.uci.edu>
2014-06-26 10:36:57 -0400

    Added dependency

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get -y install python-pip postgresql binutils libproj-dev gdal-bin postgresql-9.1-postgis postgresql-server-dev-9.1
+sudo apt-get -y install python-pip postgresql binutils libproj-dev gdal-bin postgresql-9.1-postgis postgresql-server-dev-9.1 python-dev
 
 pip install -r requirements.txt
 
